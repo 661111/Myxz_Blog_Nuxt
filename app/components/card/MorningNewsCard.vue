bc1779ffed25b6501eb2ebff352332cc4db75e1d
661111 <xiaoxin104@outlook.com>
2025-08-31 09:46:49 +0800

    更新部分样式

---
+++ app/components/card/MorningNewsCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { MorningNewsItem } from '../../types/MorningNews.ts'
+
+const props = defineProps<{
+    MorningNewsCollectionItem: MorningNewsItem
+}>()
+
+function handleClick() {
+    const url = 'https://60s-cf.viki.moe/v2/60s'
+    window.open(url, '_blank')
+}
+</script>
+
+<template>
+    <div class="MorningNewsContent" @click="handleClick">
+        <div class="head">
+            <h2 v-for="(item, index) in props.MorningNewsCollectionItem.data" :key="index">
+                {{ item.date }}
+            </h2>
+        </div>
+        <div class="body">
+            {{ MorningNewsItem.data.news }}
+        </div>
+    </div>
+</template>
+
+<style lang="css" scoped>
+</style>

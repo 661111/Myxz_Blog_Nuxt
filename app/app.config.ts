fe4126f384723920e933795e687873fdbf53d9af
661111 <xiaoxin104@outlook.com>
2025-09-03 19:40:50 +0800

    更新文章

--- app/app.config.ts
+++ app/app.config.ts
@@ -41,13 +41,13 @@ export default defineAppConfig({
 		/** 页脚版权信息，支持 <br> 换行等 HTML 标签 */
 		copyright: `© ${new Date().getFullYear()} ${blogConfig.author.name}`,
 		// /** 侧边栏底部图标导航 */
-		// iconNav: [
-        //     { icon: 'ph:house-bold', text: '个人主页', url: blogConfig.author.homepage },
-        //     // { icon: 'ri:qq-line', text: '交流群: 169994096', url: 'https://jq.qq.com/?_wv=1027&k=lQfNSeEd' },
-        //     { icon: 'ph:github-logo-bold', text: 'GitHub: 661111', url: 'https://github.com/661111' },
-        //     { icon: 'ph:rss-simple-bold', text: 'Atom订阅', url: '/atom.xml' },
-        //     { icon: 'ph:subway-bold', text: '开往', url: 'https://www.travellings.cn/go-by-clouds.html' },
-		// ] satisfies NavItem[],
+		iconNav: [
+            { icon: 'ph:house-bold', text: '个人主页', url: blogConfig.author.homepage },
+            // { icon: 'ri:qq-line', text: '交流群: 169994096', url: 'https://jq.qq.com/?_wv=1027&k=lQfNSeEd' },
+            { icon: 'ph:github-logo-bold', text: 'GitHub: 661111', url: 'https://github.com/661111' },
+            { icon: 'ph:rss-simple-bold', text: 'Atom订阅', url: '/atom.xml' },
+            { icon: 'ph:subway-bold', text: '开往', url: 'https://www.travellings.cn/go-by-clouds.html' },
+		] satisfies NavItem[],
 		/** 页脚站点地图 */
 		nav: [
 			{

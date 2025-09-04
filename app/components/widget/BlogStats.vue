ab97870a1fe1584a552cba6461d77d030738d04c
661111 <xiaoxin104@outlook.com>
2025-09-04 18:42:37 +0800

    更新文章

--- app/components/widget/BlogStats.vue
+++ app/components/widget/BlogStats.vue
@@ -6,8 +6,7 @@ const runtimeConfig = useRuntimeConfig()
 // 将服务器时区转换为博客指定时区
 const buildTime = toZonedTime(runtimeConfig.public.buildTime, appConfig.timezone)
 
-const lastUpdateTime = ref('(获取中)')
-const totalWords = ref(appConfig.component.stats.wordCount)
+const totalWords = ref(appConfig.stats.wordCount)
 const yearlyTip = ref('')
 
 const blogStats = computed(() => [{
@@ -16,7 +15,7 @@ const blogStats = computed(() => [{
 	tip: `博客于${appConfig.timeEstablished}上线`,
 }, {
 	label: '上次更新',
-	value: lastUpdateTime,
+	value: timeElapse(buildTime),
 	tip: `构建于${getLocaleDatetime(buildTime)}`,
 }, {
 	label: '总字数',
@@ -25,11 +24,7 @@ const blogStats = computed(() => [{
 }])
 
 onMounted(async () => {
-	lastUpdateTime.value = timeElapse(buildTime)
-
-	const stats = await $fetch('/api/stats').catch(() => { })
-	if (!stats)
-		return
+	const stats = await $fetch('/api/stats')
 	totalWords.value = formatNumber(stats.total.words)
 	yearlyTip.value = Object.entries(stats.annual).reverse().map(([year, item]) =>
 		`${year}年：${item.posts}篇，${formatNumber(item.words)}字`,

0f032951c4a2e0e9a9534ca3e22fa9db473d3076
661111 <xiaoxin104@outlook.com>
2025-11-30 18:50:06 +0800

    新增追更历史空白区域的显示

--- app/components/card/console/musicMain.vue
+++ app/components/card/console/musicMain.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import Aplayer from './music/index.vue'
 </script>
 
 <template>
   <div class="music-settings">
     <div class="preview-group">
       <div class="preview-container">
-        <Aplayer />
       </div>
     </div>
   </div>
